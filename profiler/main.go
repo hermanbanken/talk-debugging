76360926e2b1ad861219208336a442d5fcf477c2
Herman <herman@q42.nl>
2022-11-21 23:16:14 +0100

    add profiler demo

---
+++ profiler/main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"regexp"
+	"time"
+
+	"cloud.google.com/go/profiler"
+)
+
+func main() {
+	// Profiler initialization, best done as early as possible.
+	if err := profiler.Start(profiler.Config{
+		Service:        "myservice",
+		ServiceVersion: "1.0.1",
+		ProjectID:      "herman-codam-tmp-nov-2022",
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	rapport := time.NewTicker(5 * time.Second)
+	t := time.NewTicker(10 * time.Millisecond)
+	_ = t
+	iterations := 0
+	for {
+		select {
+		case <-rapport.C:
+			fmt.Println("Iterations", iterations)
+			iterations = 0
+		// case <-t.C:
+		default:
+			busyWork()
+			iterations++
+		}
+	}
+}
+
+func busyWork() {
+	for i := 0; i < 1000; i++ {
+		if i%2 == 0 {
+			busyWorkNested()
+		} else {
+			busyWorkOther()
+		}
+	}
+}
+
+func busyWorkNested() {
+	var dst interface{}
+	_ = json.Unmarshal([]byte(`[[[[[[[[[["nested string"]]]]]]]]]]`), &dst)
+	// do something with dst to avoid optimization
+	if len(fmt.Sprint(dst)) > 0 {
+		return
+	}
+}
+
+func busyWorkOther() {
+	if regexp.MustCompile("(a*)*c").MatchString("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa") {
+		_ = 1
+	}
+}
+
+// But remember:
+// We should forget about small efficiencies, say about 97% of the time:
+// premature optimization is the root of all evil. – Donald Knuth
