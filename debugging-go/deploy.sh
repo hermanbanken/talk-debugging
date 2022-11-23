3a94650861c8db90a68ee924a61d326923cf66c5
Herman <herman@q42.nl>
2022-11-23 13:25:57 +0100

    set sampling always on

--- debugging-go/deploy.sh
+++ debugging-go/deploy.sh
@@ -3,10 +3,10 @@ calculator=$(ko build ./calculator) && \
 equations=$(ko build ./equations) && \
 frontend=$(ko build ./frontend)
 
-gcloud run deploy calculator -q --image $calculator --region=europe-west4 --allow-unauthenticated --set-env-vars=OTEL_SERVICE_NAME=calculator,OTEL_RESOURCE_ATTRIBUTES=g.co/gae/app/module=calculator
-gcloud run deploy equations -q --image $equations --region=europe-west4 --allow-unauthenticated --set-env-vars=OTEL_SERVICE_NAME=equations,OTEL_RESOURCE_ATTRIBUTES=g.co/gae/app/module=equations
+gcloud run deploy calculator -q --image $calculator --region=europe-west4 --allow-unauthenticated --set-env-vars=OTEL_SERVICE_NAME=calculator,OTEL_RESOURCE_ATTRIBUTES=g.co/gae/app/module=calculator,OTEL_TRACES_SAMPLER=parentbased_always_on
+gcloud run deploy equations -q --image $equations --region=europe-west4 --allow-unauthenticated --set-env-vars=OTEL_SERVICE_NAME=equations,OTEL_RESOURCE_ATTRIBUTES=g.co/gae/app/module=equations,OTEL_TRACES_SAMPLER=parentbased_always_on
 
 url_calculator=$(gcloud run services describe calculator --platform managed --region=europe-west4 --format 'value(status.url)')
 url_equations=$(gcloud run services describe equations --platform managed --region=europe-west4 --format 'value(status.url)')
 
-gcloud run deploy frontend -q --image $frontend --region=europe-west4 --allow-unauthenticated --set-env-vars=URL_EQUATIONS=$url_equations,URL_CALCULATOR=$url_calculator,OTEL_SERVICE_NAME=frontend,OTEL_RESOURCE_ATTRIBUTES=g.co/gae/app/module=frontend
+gcloud run deploy frontend -q --image $frontend --region=europe-west4 --allow-unauthenticated --set-env-vars=URL_EQUATIONS=$url_equations,URL_CALCULATOR=$url_calculator,OTEL_SERVICE_NAME=frontend,OTEL_RESOURCE_ATTRIBUTES=g.co/gae/app/module=frontend,OTEL_TRACES_SAMPLER=parentbased_always_on
